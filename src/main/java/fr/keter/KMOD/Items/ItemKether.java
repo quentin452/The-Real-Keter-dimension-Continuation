87c6412178fea240d25ad7894b1aceb1c0cb5b42
QUENTIN453 <quentinlamauve@gmail.com>
2023-12-02 16:16:46 +0100

    Fix dupplication bugs with item ???
    its pretty the same bug has Arcana RPG : https://github.com/quentin452/Arcana-RPG/issues/26

--- src/main/java/fr/keter/KMOD/Items/ItemKether.java
+++ src/main/java/fr/keter/KMOD/Items/ItemKether.java
@@ -86,8 +86,11 @@ public class ItemKether extends Item {
    }
 
    public ItemStack onItemRightClick(ItemStack p_77659_1_, World p_77659_2_, EntityPlayer p_77659_3_) {
-      if (this == KMOD_Main.Pytajnik) {
-         Random random = new Random();
+       if (p_77659_3_.capabilities.isCreativeMode && (p_77659_2_.isRemote)) {
+           p_77659_3_.addChatMessage(new ChatComponentText("You must be in survival mode to use the item ??? due to a duplication issue."));
+           return p_77659_1_;
+       }
+      else if (this == KMOD_Main.Pytajnik && !p_77659_3_.capabilities.isCreativeMode) {
          int c = random.nextInt(15);
          int c1 = random.nextInt(5);
          int c2 = random.nextInt(10);
